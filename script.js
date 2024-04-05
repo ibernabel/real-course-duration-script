8782fc901886bccb973a6f3651090a3a31777a81
solufime <soporte.solufime@gmail.com>
2024-04-05 11:56:03 -0400

    Revert "✨ FEATURE: Save data to localStorage"
    This reverts commit a7816e1bfb4e81c5bdbb6ce235471bd92dce9421.

--- script.js
+++ script.js
@@ -1,67 +1,23 @@
-//Duracion real del curso mas guardado datos en local por 24 horas
-//Mas guardado en especifico por nombre del curso
-//Con Ayuda de Chat-GPT
-setTimeout(function () {
-	if (window.location.href.includes("/cursos/")) {
-		setTimeout(function () {
-			let parrafs = Array.from(document.querySelectorAll('.ContentClass-item-content > p'));
-			let duracionMinutos = 0;
-			let duracionSegundos = 0;
-
-			parrafs.forEach(function (element) {
-				let duracion = element.innerText.split(':');
-				duracionMinutos += parseInt(duracion[0]);
-				duracionSegundos += parseInt(duracion[1]);
-			});
-
-			let duracionRealMinutos = duracionMinutos + Math.floor(duracionSegundos / 60);
-			let horas = Math.floor(duracionRealMinutos / 60);
-			let minutosRestantes = duracionRealMinutos % 60;
-			let segundosRestantes = duracionSegundos % 60;
-
-			minutosRestantes = (minutosRestantes < 10) ? '0' + minutosRestantes : minutosRestantes;
-			segundosRestantes = (segundosRestantes < 10) ? '0' + segundosRestantes : segundosRestantes;
-
-			let tiempoDuracion = `${horas}:${minutosRestantes}:${segundosRestantes}`;
-			let duracionSpan = document.querySelector("#content > div.Content-wrapper.u-wrapper > div.Content-resources > div > ul > li:nth-child(1) > p > span");
-			// duracionSpan.textContent = tiempoDuracion;
-
-			let cursoNombre = obtenerNombreCurso(); // Función para obtener el nombre del curso de la URL
-			let datosGuardados = localStorage.getItem(cursoNombre);
-
-			// Comprobar si existen datos guardados y si han pasado menos de 24 horas
-			if (datosGuardados) {
-				let datos = JSON.parse(datosGuardados);
-				if (datos.horas != 0 && datos.minutosRestantes != 0 && datos.segundosRestantes != 0) {
-
-					let fechaGuardada = new Date(datos.fecha);
-					let horasPasadas = (Date.now() - fechaGuardada.getTime()) / (1000 * 60 * 60);
-					if (horasPasadas < 24) {
-						horas = datos.horas;
-						minutosRestantes = datos.minutosRestantes;
-						segundosRestantes = datos.segundosRestantes;
-					}
-				}
-			}
-
-			// Guardar los nuevos datos en el almacenamiento local
-			let datosAGuardar = {
-				horas: horas,
-				minutosRestantes: minutosRestantes,
-				segundosRestantes: segundosRestantes,
-				fecha: Date.now()
-			};
-			localStorage.setItem(cursoNombre, JSON.stringify(datosAGuardar));
-
-			console.log('La duracion real del curso es de: ' + horas + ':' + minutosRestantes + ':' + segundosRestantes);
-		}, 1000); // Retardo para ejecutar funcion de calculo duracion del curso
-	}
-}, 500); // Retardo de tiempo para leer la url
-
-function obtenerNombreCurso() {
-	let url = window.location.href;
-	let inicio = url.indexOf("/cursos/") + "/cursos/".length;
-	let fin = url.indexOf("/", inicio);
-	return url.substring(inicio, fin);
+// Here You can type your custom JavaScript...
+ if(window.location.href.includes("/cursos/")){
+  let parrafs = document.querySelectorAll('.ContentClass-item-content > p');
+  let parrafos = Array.from( parrafs );
+  let duracionTodos = parrafos.map( function( element ) { return element.innerText; });
+  let minutos = duracionTodos.map( function( element ) { return Number( element.slice( 0, 2) ) });
+  let segundos = duracionTodos.map( function( element ) { return Number( element.slice( 3, 5) ) });
+  let duracionMinutos = minutos.reduce( ( previousValue, currentValue ) => previousValue + currentValue, 0 );
+  let duracionSegundos = segundos.reduce( ( previousValue, currentValue ) => previousValue + currentValue, 0 );
+  let segundosRestantes = duracionSegundos % 60;
+  (segundosRestantes < 10 ? segundosRestantes = '0' + segundosRestantes : segundosRestantes);
+  let minutosRestantes = duracionMinutos % 60;
+  (minutosRestantes < 10 ? minutosRestantes = '0' + minutosRestantes : minutosRestantes);
+  let minutosAdicionales = Math.trunc(duracionSegundos / 60);
+  let duracionRealMinutos = duracionMinutos + minutosAdicionales;
+  let horas = Math.trunc(duracionRealMinutos / 60);
+  // 'La duracion real del curso es de: ' + horas + ' horas, ' + minutosRestantes + ' minutos, ' + segundosRestantes + ' segundos';
+  // let showDurationDivMobile = document.querySelector("#content > div:nth-child(2) > div > div.Tabs-content.Tabs-content--active > div > div > ul > li:nth-child(1) > p > span");
+  // showDurationDivMobile.innerText = horas + ':' + minutosRestantes + ' Horas';
+  // console.log( horas + ':' + minutosRestantes + ' Horas');
+  // console.log( 'La duracion real del curso es de: ' + horas + ' horas, ' + minutosRestantes + ' minutos, ' + segundosRestantes + ' segundos');
+  console.log( 'La duracion real del curso es de: ' + horas + ':' + minutosRestantes + ':' + segundosRestantes);
 }
-
